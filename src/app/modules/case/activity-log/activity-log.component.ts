2b3bdbfa7162c4d33f5e746dc696f1806d16a800
Seshapu, Srinivasarao (ss139t) <ss139t@att.com>
2018-12-08 10:21:19 +0530

    [ 08-DEC-2018 ] Fixed Activity log date issue and pushed code

--- src/app/modules/case/activity-log/activity-log.component.ts
+++ src/app/modules/case/activity-log/activity-log.component.ts
@@ -100,7 +100,7 @@ setDefault(val){
     if(this.firstOrder != 'Create Date'){
       queryInput = this.userTextInput;
     }else{
-      queryInput = this.userInput.getDay() +'/' +this.userInput.getMonth()+'/'+this.userInput.getFullYear();
+      queryInput = this.userInput.getDate() +'/' +this.userInput.getMonth()+'/'+this.userInput.getFullYear();
     }
     console.log(queryInput);
     let sortOpt = this.thirdList;
